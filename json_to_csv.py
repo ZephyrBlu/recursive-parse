971fc19afec76fd2bfa04a318f281d527c637676
ZephyrBlu <lukejholroyd@gmail.com>
2019-12-18 00:15:14 +1300

    Initial commit

--- json_to_csv.py
+++ json_to_csv.py
@@ -4,38 +4,36 @@ import csv
 """
 data schema:
 
-GameID: int (UUID),
-Map: str,
+GameID: uuid
+Map: str
 Duration: float (minutes)
-Group: char/null,
-PlayerName: str,
-IsWinner: bool,
+PlayerName: str
+IsWinner: bool
 Race: str ('Protoss', 'Terran', 'Zerg')
-UnitName: str,
-Produced: int,
-Killed: int,
-
+UnitName: str
+BirthTime: float (minutes)
+DeathTime: float (minutes)
 """
 
-with open('match_info.json', 'r', encoding='utf-8') as data:
+with open('match_info_new.json', 'r', encoding='utf-8') as data:
     match_info = json.load(data)['match_info']
 
 csv_rows = [(
     'GameID',
     'Map',
     'Duration',
-    'Group',
     'PlayerName',
     'IsWinner',
     'Race',
     'UnitName',
-    'Produced',
-    'Killed',
+    'BirthTime',
+    'DeathTime',
+    'Group',
 )]
 
 for unit_record in match_info:
     csv_rows.append(tuple(value for value in unit_record.values()))
 
-with open('match_info.csv', 'w', encoding='utf-8') as output:
+with open('match_info_new.csv', 'w', encoding='utf-8') as output:
     writer = csv.writer(output, lineterminator='\n')
     writer.writerows(csv_rows)
